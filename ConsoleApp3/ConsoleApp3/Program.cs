fff6e75e5bf1acdc98073304d33531495c967990
NikitaDanshin415 <gigobit2011@yandex.ru>
2018-11-16 18:26:04 +0500

    Update Program.cs

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,31 +1,51 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApplication15
 {
     class Program
     {
         static void Main(string[] args)
         {
             float b = 2, a = 1, k = 10;
-            for(double x=1; x<=2; x +=(b-a)/k ){
-                Console.Write($"X = {x} |");               
-                double s=0;
 
-                for (int n=0;n<=15;n++)
+            for (double x = 1; x <= 2; x += (b - a) / k)
+            {
+                Console.Write($"X = {x} |");
+                double s = 0;
+
+                for (int n = 0; n <= 15; n++)
                 {
-                    s += Math.Pow(x,n)/(int)fa(n);
+                    s += Math.Pow(x, n) / (int)fa(n);
                 }
                 Console.Write($"SN = {s} |");
 
 
                 Console.Write($"Y = {Math.Pow(Math.E, x)} |");
+
+                const double eps = 0.0001;
+                double se = 1;
+                double fact2 = 1;
+                int m = 1;
+                double currentTerm = 1;
+
+                while (currentTerm > eps)
+                {
+                    fact2 = fa(m);
+                    currentTerm = Math.Pow(x, m) / (int)fa(m);
+                    se = se + currentTerm;
+                    m++;
+                }
+
+                Console.Write($"SE = {se} |");
                 Console.WriteLine();
+
+
             }
-          
+
         }
 
         static double fa(double x)
@@ -37,6 +57,5 @@ namespace ConsoleApp1
             }
             return result;
         }
-
     }
 }
