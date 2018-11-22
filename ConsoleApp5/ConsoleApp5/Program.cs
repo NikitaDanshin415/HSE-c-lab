5c9464b6d728ca7c1412f3614335af75ba77f38f
NikitaDanshin415 <gigobit2011@yandex.ru>
2018-11-22 22:12:46 +0500

    Update Program.cs

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,28 +13,33 @@ namespace ConsoleApp5
         {
             int[] nums;
             int[,] numsQ;
-            int lg;
-            Console.Write("Введите длину массива = ");
-            while (!int.TryParse(Console.ReadLine(), out lg))
-            {
-                Console.Write("Ошибка, введите длину массива = ");
-            }
-            nums = CreateArray(lg);
-            printArray(nums);
-            int k;
-            Console.Write("Введите K = ");
-            while (!int.TryParse(Console.ReadLine(), out k))
-            {
-                Console.Write("Ошибка, введите K = ");
-            }
-            nums = dellElem(nums, k);
-            printArray(nums);
+            int[][] numsR;
+            //int lg;
+            //Console.Write("Введите длину массива = ");
+            //while (!int.TryParse(Console.ReadLine(), out lg))
+            //{
+            //    Console.Write("Ошибка, введите длину массива = ");
+            //}
+            //nums = CreateArray(lg);
+            //printArray(nums);
+            //int k;
+            //Console.Write("Введите K = ");
+            //while (!int.TryParse(Console.ReadLine(), out k))
+            //{
+            //    Console.Write("Ошибка, введите K = ");
+            //}
+            //nums = dellElem(nums, k);
+            //printArray(nums);
 
 
             numsQ = CreateQuadArray(5, 6);
             printQuadArray(numsQ);
             numsQ = addColumnInStart(numsQ);
             printQuadArray(numsQ);
+            numsR = CreateaRaggedArray(5, 5);
+            printRaggedArray(numsR);
+            numsR = dellStrings(numsR, 3, 4);
+            printRaggedArray(numsR);
 
         }
 
@@ -67,6 +72,22 @@ namespace ConsoleApp5
             Console.WriteLine();
         }
 
+        static void printRaggedArray(int[][] a)
+        {
+            Console.Write("Элементы массива:");
+            Console.WriteLine();
+            foreach (int[] row in a)
+            {
+                foreach (int number in row)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(String.Format("{0,3} ", number));
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+        }
+
         public static int[] CreateArray(int lg)
         {
 
@@ -90,6 +111,24 @@ namespace ConsoleApp5
                 for (int j = 0; j < lg2; j++)
                 {
                     nums[i, j] = rand.Next(-100, 100);
+                }
+            }
+
+            return nums;
+        }
+
+        public static int[][] CreateaRaggedArray(int lg1, int lg2)
+        {
+            int[][] nums = new int[lg1][];
+            Random rand = new Random();
+
+            for (int i = 0; i < lg1; i++)
+            {
+                int lg3 = rand.Next(1, lg2);
+                nums[i] = new int[lg3];
+                for (int j = 0; j < lg3; j++)
+                {
+                    nums[i][j] = rand.Next(-100, 100);
                 }
             }
 
@@ -141,23 +180,39 @@ namespace ConsoleApp5
             return nums;
         }
 
-        public static int[,] dellStrings(int[,] a, int k1, int k2)
+
+
+        public static int[][] dellStrings(int[][] a, int k1, int k2)
         {
-            int lg1 = a.GetLength(0) - (k2 - k1 + 1);    //строки i
-            int lg2 = a.GetLength(1);  //столбцы j 
+            int lg = 0;
 
-            int[,] nums = new int[lg1, lg2];
-            Random rand = new Random();
-
-            for (int i = 0; i < lg1; i++)
+            if (k1 < k2)
             {
-                for (int j = 0; j < lg2; j++)
+                lg = a.Length - (k2 - k1);
+            }
+            else if (k1 > k2)
+            {
+                Console.WriteLine("Число к1 должно быть меньше к2");
+            }
+            else
+            {
+                lg = a.Length - 1;
+            }
+
+            int[][] nums = new int[lg][];
+            int j = 0;
+            for (int i = 0; i < lg; i++)
+            {
+                if (i!=2)
                 {
-                    if (i <= k1 && j >= k2)
-                    {
-                        nums[i, j] = a[i, j];
-                    }
+                    nums[j] = a[i];
+                    j++;
                 }
+                else
+                {
+                    j--;
+                }
+                
             }
             return nums;
         }
